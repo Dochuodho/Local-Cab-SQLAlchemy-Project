6c3dfc312ae1ff7d98f5ee4beb30ae2f2eb00505
Ochuodho <ochuodhoderrick@gmail.com>
2023-10-05 21:13:55 +0300

    alembic setup

--- lib/cab.py
+++ lib/cab.py
@@ -1,7 +1,8 @@
 #lib/cab.py
 #!/usr/bin/env python3
-from sqlalchemy import Column,Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy import Column,Integer, String, create_engine,desc, CheckConstraint, PrimaryKeyConstraint, UniqueConstraint, Index, DateTime 
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
 
@@ -11,9 +12,27 @@ class Customer(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     gender = Column(String())
+    email = Column(String())
+    booking_time = Column(DateTime(), default=datetime.now())
 
+    def __repr__(self):
+        return f"The rider's name is {self.name} and the booking time is {self.booking_time}"
 
-engine = create_engine('sqlite:///cab.db')
-Base.metadata.create_all(engine)
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///cab.db')
+    Base.metadata.create_all(engine)
+
+#configuring a session
+Session = sessionmaker(bind=engine)
+#use  'Session' class to create 'session' object
+session = Session()
+
+derrick = Customer(name="Derrick", gender="male", email="derrick@gmail.com") 
+whitney = Customer(name="Whitney", gender="female", email="whitney@gmail.com") 
+wendy = Customer(name="Wendy", gender="female", email="wendy@gmail.com") 
+
+session.add_all([derrick, whitney,wendy])
+session.commit()
+
 
 
